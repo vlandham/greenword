6a6d196a473653d4844725f60369d3d0bb02d721
Jim Vallandingham <vlandham@gmail.com>
2007-04-12 15:52:10 +0000

    initial

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.2.2'
+RAILS_GEM_VERSION = '1.2.3'
 
 # Here are some defaults for the Settings table:
 module SettingsDefaults
