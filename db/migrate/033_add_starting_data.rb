e77f4735874fb41ce8ec378bef0d4173ebd89462
Jim Vallandingham <vlandham@gmail.com>
2007-04-12 17:40:19 +0000

    added globalize 1.2

--- db/migrate/033_add_starting_data.rb
+++ db/migrate/033_add_starting_data.rb
@@ -1,7 +1,8 @@
 class AddStartingData < ActiveRecord::Migration
   def self.up
     User.create( :first_name => "Admin", :last_name => "Admin", :login => "admin@ku.edu", 
-                 :email => "admin@ku.edu", :password => "pasword", :admitted => 1, :admin => 1)
+                 :email => "admin@ku.edu", :password => "password", :admitted => 1, :admin => 1
+                 :semester_id => 1)
     Semester.create( :name => "First Semester", :freeze => 0)
   end
 
