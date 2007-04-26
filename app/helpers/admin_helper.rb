66fb720f26d9b918d8bde072f42872a2184e31a8
Jim Vallandingham <vlandham@gmail.com>
2007-04-26 17:51:42 +0000

    added ferret

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -6,6 +6,22 @@ module AdminHelper
   
   def link_to_discussion
      link_to icon("comments"), :controller => :discussion, :action => :index
-   end
+  end
+  
+  def excerpt_all_words(text, phrase, radius = 100, excerpt_string = "...")
+    phrase_array = phrase.split(' ')
+ 
+    phrase_array.each_with_index do |word, index|
+      excerpt(text,phrase_array[index],radius,excerpt_string) 
+    end
+  end
+  
+  def highlight_all_words(text, phrase, highlighter = '<strong class="highlight">\1</strong>')
+        phrase_array = phrase.split(' ')
+        phrase_array.each do |phrase_piece|
+            text =highlight(text,phrase_piece, highlighter)
+        end
+        text
+  end
   
 end
